de5c58f7943f2395118bb31f5e1ebb7800978ac6
JariAtBlitzIt <jari@blitzit.io>
2021-03-21 10:05:24 -0700

    Clean Default Base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+// https://www.youtube.com/watch?v=2-LISBTczQE
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
